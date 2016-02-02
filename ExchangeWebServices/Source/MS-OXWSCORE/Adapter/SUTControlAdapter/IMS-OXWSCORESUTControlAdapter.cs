d5e42b8b538df9966ade7401367cb4c89b141f9f
YanfeiHan <v-yahan@microsoft.com>
2016-02-02 15:22:27 +0800

    Update code according to review comments.

---
+++ ExchangeWebServices/Source/MS-OXWSCORE/Adapter/SUTControlAdapter/IMS-OXWSCORESUTControlAdapter.cs
@@ -0,0 +1,19 @@
+namespace Microsoft.Protocols.TestSuites.MS_OXWSCORE
+{
+    using Microsoft.Protocols.TestTools;
+
+    /// <summary>
+    /// MS-ASCMD SUT control adapter interface
+    /// </summary>
+    public interface IMS_OXWSCORESUTControlAdapter : IAdapter
+    {
+        /// <summary>
+        /// Sets junk email sender to blocked sender list.
+        /// </summary>
+        /// <param name="itemSenter">The sender of mailbox.</param>
+
+        [MethodHelp("Log on to the server (serverComputerName) with the specified user account (userName, userPassword, userDomain), " +
+            "and set junk email sender to blocked sender list. ")]
+        string GetMailboxJunkEmailConfiguration();
+    }
+}
