6c6ac26208f91f6bc57c3f93f9913b4bc891915a
YanfeiHan <v-yahan@microsoft.com>
2016-02-22 16:33:43 +0800

    Update S04_TC18

--- ExchangeWebServices/Source/MS-OXWSCORE/Adapter/SUTControlAdapter/IMS-OXWSCORESUTControlAdapter.cs
+++ ExchangeWebServices/Source/MS-OXWSCORE/Adapter/SUTControlAdapter/IMS-OXWSCORESUTControlAdapter.cs
@@ -3,16 +3,15 @@ namespace Microsoft.Protocols.TestSuites.MS_OXWSCORE
     using Microsoft.Protocols.TestTools;
 
     /// <summary>
-    /// MS-ASCMD SUT control adapter interface
+    /// MS-OXWSCORE SUT control adapter interface
     /// </summary>
     public interface IMS_OXWSCORESUTControlAdapter : IAdapter
     {
         /// <summary>
         /// Gets junk email sender in blocked sender list.
         /// </summary>
-        /// <param name="Organizer">The use account of organizer.</param>
-
-        [MethodHelp("Get junk email sender in blocked sender list of the use account (Organizer). ")]
-        string GetMailboxJunkEmailConfiguration(string Organizer);
+        /// <param name="UserName">The user account of organizer.</param>
+        [MethodHelp("Get junk email sender in blocked sender list of the user account (UserName). ")]
+        string GetMailboxJunkEmailConfiguration(string UserName);
     }
 }
--- ExchangeWebServices/Source/MS-OXWSCORE/Adapter/SUTControlAdapter/Implementation/GetMailboxJunkEmailConfiguration.ps1
+++ ExchangeWebServices/Source/MS-OXWSCORE/Adapter/SUTControlAdapter/Implementation/GetMailboxJunkEmailConfiguration.ps1
@@ -1,26 +1,26 @@
 $script:ErrorActionPreference = "Stop"
 $SutComputerName = $PtfPropSutComputerName
-$UserName = "$PtfPropDomain\$PtfPropUserName"
+$Admin = "$PtfPropDomain\$PtfPropUserName"
 $Password = $PtfPropPassword
 $credentialSecurePassword = ConvertTo-SecureString $Password -AsPlainText -Force
-$credential = new-object Management.Automation.PSCredential($UserName,$credentialSecurePassword)
+$credential = new-object Management.Automation.PSCredential($Admin,$credentialSecurePassword)
 
 invoke-command -computername $SutComputerName -Credential $credential  -ScriptBLock{
 param(
 	[string]$SutComputerName,
-	[string]$UserName,
+	[string]$Admin,
 	[string]$Password,
-	[string]$Organizer
+	[string]$UserName
       )
 
     $credentialSecurePassword = ConvertTo-SecureString $Password -AsPlainText -Force
-    $credential = new-object Management.Automation.PSCredential($UserName,$credentialSecurePassword)
+    $credential = new-object Management.Automation.PSCredential($Admin,$credentialSecurePassword)
     $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://$SutComputerName/PowerShell/ -Credential $credential -Authentication Kerberos
     Import-PSSession $session
 
-$result = Get-MailboxJunkEmailConfiguration $Organizer | select BlockedSendersAndDomains
+$result = Get-MailboxJunkEmailConfiguration $UserName | select BlockedSendersAndDomains
 [string]$BlockSender =$result.BlockedSendersAndDomains
 
     Remove-PSSession $session
 return $BlockSender
-} -ArgumentList $SutComputerName, $UserName, $Password, $Organizer
+} -ArgumentList $SutComputerName, $UserName, $Password, $UserName
