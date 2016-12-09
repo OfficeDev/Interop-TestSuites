181c5eeb1ad011894fbdbf57dc06845b0965cc95
TingtingWen <v-tiwe@microsoft.com>
2016-12-09 17:15:38 +0800

    [MS-FSSHTTP]: Add 3+ cases for VersioningSubRequestDataType,VersioningSubRequestType,VersioningSubResponseDataType,VersioningSubResponseType,VersioningUserTableType,VersioningVersionListType,UserDataType,FileVersionDataType,VersioningRequestTypes

--- FileSyncandWOPI/Source/SharedTestSuite/SharedAdapter/CaptureCode/FSSHTTP/MsfsshttpVersioningCapture.cs
+++ FileSyncandWOPI/Source/SharedTestSuite/SharedAdapter/CaptureCode/FSSHTTP/MsfsshttpVersioningCapture.cs
@@ -16,8 +16,161 @@ namespace Microsoft.Protocols.TestSuites.SharedAdapter
         /// <param name="site">Instance of ITestSite</param>
         public static void ValidateVersioningSubResponse(VersioningSubResponseType versioningSubResponse, ITestSite site)
         {
+            // Verify MS-FSSHTTP requirement: MS-FSSHTTP_R11154
+            site.CaptureRequirement(
+                "MS-FSSHTTP",
+                11154,
+                @"[In VersioningSubResponseType] 
+                 <xs:complexType name=""VersioningSubResponseType"">
+                   < xs:complexContent >
+                     < xs:extension base = ""tns:SubResponseType"" >
+                       < xs:sequence minOccurs = ""0"" maxOccurs = ""1"" >
+                          < xs:element name = ""SubResponseData"" type = ""tns:VersioningSubResponseDataType"" />
+                       </ xs:sequence >
+                     </ xs:extension >
+                   </ xs:complexContent >
+                 </ xs:complexType > ");
 
+            // Verify MS-FSSHTTP requirement: MS-FSSHTTP_R11155
+            site.CaptureRequirement(
+                "MS-FSSHTTP",
+                11155,
+                @"[In VersioningSubResponseType] SubResponseData: A VersioningSubResponseDataType that specifies versioning related information provided by the protocol server that was requested as part of the versioning subrequest.");
+
+            // Verify MS-FSSHTTP requirement: MS-FSSHTTP_R11144
+            site.CaptureRequirement(
+                "MS-FSSHTTP",
+                11144,
+                @"[In VersioningSubResponseDataType] 
+                <xs:complexType name=""VersioningSubResponseDataType"">
+                    <xs:sequence minOccurs=""0"" maxOccurs=""1"">
+                        <xs:sequence minOccurs=""0"" maxOccurs=""1"">
+                            <xs:element name=""UserTable"" type=""tns:VersioningUserTableType""/>
+                        </xs:sequence>
+                        <xs:element name=""Versions"" type=""tns:VersioningVersionListType""/>
+                    </xs:sequence>
+                </xs:complexType>");
+
+            if (versioningSubResponse.SubResponseData.UserTable != null)
+            {
+                // Verify MS-FSSHTTP requirement: MS-FSSHTTP_R11145
+                site.CaptureRequirement(
+                    "MS-FSSHTTP",
+                    11145,
+                    @"[In VersioningSubResponseDataType] UserTable: An element of type VersioningUserTableType (section 2.3.1.40) that specifies data for the users represented in the version list.");
+
+                ValidateVersioningUserTableType(versioningSubResponse.SubResponseData.UserTable, site);
+
+
+            }
+
+            if (versioningSubResponse.SubResponseData.Versions != null)
+            {
+                // Verify MS-FSSHTTP requirement: MS-FSSHTTP_R11147
+                site.CaptureRequirement(
+                    "MS-FSSHTTP",
+                    11147,
+                    @"[In VersioningSubResponseDataType] Versions: An element of type VersioningVersionListType (section 2.3.1.41) that specifies the list of versions of this file that exist on the server.");
+
+                ValidateVersioningVersionListType(versioningSubResponse.SubResponseData.Versions, site);
+            }
         }
 
+        /// <summary>
+        /// Capture requirements related with VersioningUserTableType.
+        /// </summary>
+        /// <param name="userTable">The VersioningUserTableType</param>
+        /// <param name="site">Instance of ITestSite</param>
+        private static void ValidateVersioningUserTableType(VersioningUserTableType userTable, ITestSite site)
+        {
+
+            // Verify MS-FSSHTTP requirement: MS-FSSHTTP_R11159
+            site.CaptureRequirement(
+                "MS-FSSHTTP",
+                11159,
+                @"[In VersioningUserTableType] User: An element of type UserDataType (section 2.3.1.42) which describes a single user.");
+
+            // Verify MS-FSSHTTP requirement: MS-FSSHTTP_R11164
+            site.CaptureRequirement(
+                "MS-FSSHTTP",
+                11164,
+                @"[In UserDataType] 
+                     <xs:complexType name=""UserDataType"">
+                        < s:attribute name = ""UserId"" type = ""xs:integer"" use = ""required"" />
+                        < s:attribute name = ""UserLogin"" type = ""xs:UserLoginType"" use = ""required"" />
+                        < s:attribute name = ""UserName"" type = ""xs:UserNameType"" use = ""optional"" />
+                        < s:attribute name = ""UserEmailAddress"" type = ""s:string"" use = ""optional"" />
+                     </ xs:complexType > ");
+
+            // Verify MS-FSSHTTP requirement: MS-FSSHTTP_R11165
+            site.CaptureRequirement(
+                "MS-FSSHTTP",
+                11165,
+                @"[In UserDataType] UserId: An integer that uniquely specifies the user in this user table.");
+
+            // Verify MS-FSSHTTP requirement: MS-FSSHTTP_R11167
+            site.CaptureRequirement(
+                "MS-FSSHTTP",
+                11167,
+                @"[In UserDataType] UserLogin: A UserLoginType that specifies the user login alias of  the protocol client.");
+
+
+            if (!string.IsNullOrEmpty(userTable.User[0].UserName))
+            {
+                // Verify MS-FSSHTTP requirement: MS-FSSHTTP_R11169
+                site.CaptureRequirement(
+                    "MS-FSSHTTP",
+                    11169,
+                    @"[In UserDataType] UserName: A UserNameType that specifies the user name for the protocol client.");
+            }
+
+            if (!string.IsNullOrEmpty(userTable.User[0].UserEmailAddress))
+            {
+                // Verify MS-FSSHTTP requirement: MS-FSSHTTP_R11171
+                site.CaptureRequirement(
+                    "MS-FSSHTTP",
+                    11171,
+                    @"[In UserDataType] UserEmailAddress: A string that specifies the email address associated with the protocol client.");
+
+                // Verify MS-FSSHTTP requirement: MS-FSSHTTP_R11172
+                site.CaptureRequirementIfIsTrue(
+                    AdapterHelper.IsValidEmailAddr(userTable.User[0].UserEmailAddress),
+                    "MS-FSSHTTP",
+                    11172,
+                    @"[In UserDataType] The format of the email address MUST be as specified in [RFC2822] section 3.4.1.");
+            }
+        }
+
+        /// <summary>
+        /// Capture requirements related with VersioningVersionListType.
+        /// </summary>
+        /// <param name="versionList">The VersioningVersionListType</param>
+        /// <param name="site">Instance of ITestSite</param>
+        private static void ValidateVersioningVersionListType(VersioningVersionListType versionList, ITestSite site)
+        {
+            // Verify MS-FSSHTTP requirement: MS-FSSHTTP_R11162
+            site.CaptureRequirement(
+                "MS-FSSHTTP",
+                11162,
+                @"[In VersioningVersionListType] Version: An element of type FileVersionDataType (section 2.3.1.43) which describes a single version of the file on the server.");
+
+            // Verify MS-FSSHTTP requirement: MS-FSSHTTP_R11176
+            site.CaptureRequirement(
+                "MS-FSSHTTP",
+                11176,
+                @"[In FileVersionDataType] Number: A FileVersionNumberType (section 2.2.5.15) that specifies the unique version number of the version of the file.");
+
+            // Verify MS-FSSHTTP requirement: MS-FSSHTTP_R11177
+            site.CaptureRequirement(
+                "MS-FSSHTTP",
+                11177,
+                @"[In FileVersionDataType] LastModifiedTime: A positive integer that specifies the last modified time of the version of the file, which is expressed as a tick count.");
+
+            // Verify MS-FSSHTTP requirement: MS-FSSHTTP_R11182
+            site.CaptureRequirement(
+                "MS-FSSHTTP",
+                11182,
+                @"[In FileVersionDataType] Event: A FileVersionEventDataType that represents an event that happened to the version of the file.");
+        }
     }
 }
--- FileSyncandWOPI/Source/SharedTestSuite/SharedTestCase/S16_Versioning.cs
+++ FileSyncandWOPI/Source/SharedTestSuite/SharedTestCase/S16_Versioning.cs
@@ -37,14 +37,16 @@ namespace Microsoft.Protocols.TestSuites.SharedTestSuite
 
         #region Test Case Initialization
 
+        DateTime time = DateTime.Now.AddDays(-1);
+
         /// <summary>
         /// A method used to initialize the test class.
         /// </summary>
         [TestInitialize]
         public void S16_VersioningInitialization()
         {
-            // Initialize the default file URL, for this scenario, the target file URL should not need unique for each test case, just using the preparing one.
-            this.DefaultFileUrl = Common.GetConfigurationPropertyValue("NormalFile", this.Site);
+            this.DefaultFileUrl = this.PrepareFile();
+            time = DateTime.UtcNow;
         }
 
         #endregion
@@ -70,6 +72,106 @@ namespace Microsoft.Protocols.TestSuites.SharedTestSuite
             this.Site.Assert.IsNotNull(versioningSubResponse, "The object 'versioningSubResponse' should not be null.");
             this.Site.Assert.IsNotNull(versioningSubResponse.ErrorCode, "The object 'versioningSubResponse.ErrorCode' should not be null.");
 
+            if (SharedContext.Current.IsMsFsshttpRequirementsCaptured)
+            {
+                // Verify MS-FSSHTTP requirement: MS-FSSHTTP_R11146
+                Site.CaptureRequirementIfIsNotNull(
+                    versioningSubResponse.SubResponseData.UserTable,
+                    "MS-FSSHTTP",
+                    11146,
+                    @"[In VersioningSubResponseDataType] The UserTable element MUST be included in the response if the SubResponseType of the parent VersioningSubResponseType is of type ""GetVersionList.""");
+
+                // Verify MS-FSSHTTP requirement: MS-FSSHTTP_R11148
+                Site.CaptureRequirementIfIsNotNull(
+                    versioningSubResponse.SubResponseData.Versions,
+                    "MS-FSSHTTP",
+                    11148,
+                    @"[In VersioningSubResponseDataType] The Versions element MUST be included in the response if the SubResponseType of the parent VersioningSubResponseType is of type ""GetVersionList.""");
+
+                // Verify MS-FSSHTTP requirement: MS-FSSHTTP_R11150
+                // This requirement can be captured directly after capturing MS-FSSHTTP_R11146 and MS-FSSHTTP_R11148
+                Site.CaptureRequirement(
+                    "MS-FSSHTTP",
+                    11150,
+                    @"[In VersioningSubResponseType] In the case of success, it contains information requested as part of a versioning subrequest.");
+
+                string expectLastModifiedTime =
+                    ((long)(time - new DateTime(1601, 1, 1, 0, 0, 0)).TotalSeconds * 10000000).ToString();
+
+                // Verify MS-FSSHTTP requirement: MS-FSSHTTP_R11179
+                Site.CaptureRequirementIfAreEqual<string>(
+                    expectLastModifiedTime,
+                    versioningSubResponse.SubResponseData.Versions.Version[0].LastModifiedTime,
+                    "MS-FSSHTTP",
+                    11179,
+                    @"[In FileVersionDataType] LastModifiedTime specifies the number of 100-nanosecond intervals that have elapsed since 00:00:00 on January 1, 1601, which MUST be Coordinated Universal Time (UTC).");
+
+                // Verify MS-FSSHTTP requirement: MS-FSSHTTP_R11178
+                // Calculate expected last modified time by multiple the time span with ten million, this requirement can be captured.
+                Site.CaptureRequirementIfAreEqual<string>(
+                    expectLastModifiedTime,
+                    versioningSubResponse.SubResponseData.Versions.Version[0].LastModifiedTime,
+                    "MS-FSSHTTP",
+                    11178,
+                    @"[In FileVersionDataType] A single tick represents 100 nanoseconds, or one ten-millionth of a second.");
+
+                // Verify MS-FSSHTTP requirement: MS-FSSHTTP_R11180
+                Site.CaptureRequirementIfAreEqual<string>(
+                    versioningSubResponse.SubResponseData.UserTable.User[0].UserId,
+                    versioningSubResponse.SubResponseData.Versions.Version[0].UserId,
+                    "MS-FSSHTTP",
+                    11180,
+                    @"[In FileVersionDataType] UserId: An integer that specifies the user that last modified the version of the file.");
+
+                // Verify MS-FSSHTTP requirement: MS-FSSHTTP_R11181
+                Site.CaptureRequirementIfAreEqual<string>(
+                    versioningSubResponse.SubResponseData.UserTable.User[0].UserId,
+                    versioningSubResponse.SubResponseData.Versions.Version[0].UserId,
+                    "MS-FSSHTTP",
+                    11181,
+                    @"[In FileVersionDataType] The number MUST match the UserId attribute of a UserDataType (section 2.3.1.42) described in the VersioningUserTableType in the current VersioningSubResponseDataType.");
+            }
+            else
+            {
+                Site.Assert.IsNotNull(
+                    versioningSubResponse.SubResponseData.UserTable,
+                    @"The UserTable element must be included in the response if the SubResponseType of the parent VersioningSubResponseType is of type ""GetVersionList.""");
+
+                Site.Assert.IsNotNull(
+                    versioningSubResponse.SubResponseData.Versions,
+                    @"The UserTable element must be included in the response if the SubResponseType of the parent VersioningSubResponseType is of type ""GetVersionList.""");
+            }
+        }
+
+        /// <summary>
+        /// A method used to verify that Versioning sub-request failed with empty url.
+        /// </summary>
+        [TestCategory("SHAREDTESTCASE"), TestMethod()]
+        public void TestCase_S16_TC02_Versioning_EmptyUrl()
+        {
+            // Initialize the service
+            this.InitializeContext(this.DefaultFileUrl, this.UserName01, this.Password01, this.Domain);
+
+            VersioningSubRequestType versioningSubRequest = SharedTestSuiteHelper.CreateVersioningSubRequest(SequenceNumberGenerator.GetCurrentToken(), VersioningRequestTypes.GetVersionList, null, this.Site);
+            CellStorageResponse cellStoreageResponse = Adapter.CellStorageRequest(string.Empty, new SubRequestType[] { versioningSubRequest });
+
+            if (SharedContext.Current.IsMsFsshttpRequirementsCaptured)
+            {
+                // Verify MS-FSSHTTP requirement: MS-FSSHTTP_R11151
+                Site.CaptureRequirementIfAreNotEqual<GenericErrorCodeTypes>(
+                    GenericErrorCodeTypes.Success,
+                    cellStoreageResponse.ResponseVersion.ErrorCode,
+                    "MS-FSSHTTP",
+                    11151,
+                    @"[In VersioningSubResponseType] In the case of failure, the ErrorCode attribute that is part of a SubResponse element specifies the error code result for this subrequest.");
+            }
+            else
+            {
+                Site.Assert.AreNotEqual<GenericErrorCodeTypes>(
+                    GenericErrorCodeTypes.Success,
+                    cellStoreageResponse.ResponseVersion.ErrorCode,
+                    "Error should occur if call versioning request with empty url.");
+            }
         }
 
         #endregion
