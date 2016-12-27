2eb0eb2c771e2bed09219c4f1267650520ca2e58
YanfeiHan <v-yahan@microsoft.com>
2016-12-27 16:48:23 +0800

    Add 3+ cases for FileOperationSubRequestDataType,FileOperationSubRequestType,FileOperationSubResponseType,FileOperationRequestTypes

--- FileSyncandWOPI/Source/SharedTestSuite/SharedTestCase/S17_FileOperation.cs
+++ FileSyncandWOPI/Source/SharedTestSuite/SharedTestCase/S17_FileOperation.cs
@@ -198,8 +198,6 @@ namespace Microsoft.Protocols.TestSuites.SharedTestSuite
                 Site.Assert.AreEqual<ErrorCodeType>(
                     ErrorCodeType.InvalidArgument,
                     errorCode,
-                    "MS-FSSHTTP",
-                    11267,
                     @"[In Appendix B: Product Behavior] If the specified attributes[FileOperationRequestType attribute] are not provided, the implementation does return an ""InvalidArgument"" error code as part of the SubResponseData element associated with the file opeartion subresponse. (Microsoft Office 2010 suites/Microsoft SharePoint Foundation 2010/Microsoft SharePoint Server 2010/Microsoft SharePoint Workspace 2010/Microsoft Office 2016/Microsoft SharePoint Server 2016 follow this behavior.)");
             }
         }
