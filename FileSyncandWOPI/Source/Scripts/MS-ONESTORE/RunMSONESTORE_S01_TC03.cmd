0e2efbaa5eeea4238183fe1002014f4733e8016f
v-xinwli@microsoft.com <Password01!>
2019-03-29 16:24:55 +0800

    [MS-ONESTORE] add cmd files for MS-ONESTORE test suite.

---
+++ FileSyncandWOPI/Source/Scripts/MS-ONESTORE/RunMSONESTORE_S01_TC03.cmd
@@ -0,0 +1,4 @@
+@echo off
+pushd %~dp0
+"%VS120COMNTOOLS%..\IDE\mstest" /test:Microsoft.Protocols.TestSuites.MS_ONESTORE.S01_TransmissionByFSSHTTP.MSONESTORE_S01_TC03_QueryOneFileWithoutFileData /testcontainer:..\..\MS-ONESTORE\TestSuite\bin\Debug\MS-ONESTORE_TestSuite.dll /runconfig:..\..\MS-ONESTORE\MS-ONESTORE.testsettings /unique
+pause
---
+++ FileSyncandWOPI/Source/Scripts/MS-ONESTORE/RunMSONESTORE_S02_TC05.cmd
@@ -0,0 +1,4 @@
+@echo off
+pushd %~dp0
+"%VS120COMNTOOLS%..\IDE\mstest" /test:Microsoft.Protocols.TestSuites.MS_ONESTORE.S02_OneNoteRevisionStore.MSONESTORE_S02_TC05_LoadOneNoteWithLargeReferences /testcontainer:..\..\MS-ONESTORE\TestSuite\bin\Debug\MS-ONESTORE_TestSuite.dll /runconfig:..\..\MS-ONESTORE\MS-ONESTORE.testsettings /unique
+pause
